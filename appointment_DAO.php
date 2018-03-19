c6f8b9e9492359d1a50dff6496cf84459197a94c
Zach <zbrule@gmail.com>
2018-03-19 16:35:59 -0400

    Updated student.php, created send_email.php

---
+++ appointment_DAO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zach
+ * Date: 3/19/2018
+ * Time: 1:17 PM
+ */
+
+include_once $_SERVER['DOCUMENT_ROOT'] . "/assets/class/sql/connection.php";
+
+class appointment_DAO{
+
+
+
+}
